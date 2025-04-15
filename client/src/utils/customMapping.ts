c21eb481033f283598702b72a84562aba943cf7e
Suyash Kamath <suyashkamath9@gmail.com>
2025-04-15 18:36:26 +0530

    Second Commit

--- client/src/utils/customMapping.ts
+++ client/src/utils/customMapping.ts
@@ -1,6 +1,13 @@
 const customMapping: { [key: string]: string } = {
     php: "php",
     cs: "csharp",
+    cpp: "cpp",
+    cxx: "cpp",
+    cc: "cpp",
+    c: "cpp",
+    h: "cpp",
+    hpp: "cpp",
+    hxx: "cpp",
     // Maybe add more languages here
 }
 
